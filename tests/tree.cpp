748f7b8e1d6a2bf2ace96acb4f92065fd69fa059
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-05-02 01:23:10 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -12,14 +12,12 @@ TEST_CASE("creating tree")
 TEST_CASE("insert and print tree")
 {
 	std::string input{
-		"        ----9\n"
-		"                ----8\n"
-		"            ----7\n"
-		"    ----7\n"
-		"        ----6\n"
-		"----5\n"
-		"        ----4\n"
-		"    ----4\n"
+		"        ---- 9\n"
+		"            ---- 8\n"
+		"    ---- 7\n"
+		"        ---- 6\n"
+		"---- 5\n"
+		"    ---- 4\n"
 		"        ----3\n"};
 	
 	tree_t<int> tree { 5, 7, 4, 6, 9, 7, 8, 3, 4 };
@@ -42,117 +40,37 @@ TEST_CASE("find tree")
 	REQUIRE( tree.find(0) == false );
 }
 
-TEST_CASE("remove tree")
-{
-	tree_t<int> tree { 5, 7, 4 };
-	
-	REQUIRE( tree.remove(6) == false );
-	REQUIRE( tree.remove(7) == true );
-	REQUIRE( tree.remove(5) == true );
-	REQUIRE( tree.remove(4) == true );
-}
-
 TEST_CASE("compare tree")
 {
 	tree_t<int> A { 5, 7, 4 };
-	tree_t<int> B { 5, 7, 4 };
-	tree_t<int> C { 5, 7, 4, 4 };
+	tree_t<int> B { 5, 7, 4, 4 };
+	tree_t<int> C { 5, 7, 4, 4, 6 };
 	
 	REQUIRE( (A == B) == true );
 	REQUIRE( (C == B) == false );
 }
 
-TEST_CASE("read tree")
+TEST_CASE("isEmpty tree")
 {
-	tree_t<double> tree;
+	tree_t<int> tree1;
+	tree_t<int> tree2;
 	
-	bool success = true;
-	for(std::size_t i = 0; i < 5; ++i) {
-		if (i == 0) {
-			std::string input{"+9.8"};
-			
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 1) {
-			std::string input{"="};
-			std::string output{"----9.8\n"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-			REQUIRE( output == ostream.str() );
-		}
-		else if (i == 2) {
-			std::string input{"?1"};
-			std::string output{"false"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-			
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 3) {
-			std::string input{"d"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 4) {
-			std::string input{"q"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == false );
-		}
-	}
+	tree1.insert(5);
+	tree1.insert(7);
+	tree1.insert(4);
+	
+	REQUIRE( tree1.isEmpty == false );
+	REQUIRE( tree2.isEmpty == true );
 }
 
-SCENARIO("BST delete non inserted element", "[delete]") {
-    tree_t<int> tree = {8};
-    REQUIRE( !tree.remove(4) );
-    REQUIRE( !tree.isEmpty() );
-}
 
-SCENARIO("BST delete root without children", "[delete]") {
-    tree_t<int> tree = {8};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree.isEmpty() );
-}
-
-SCENARIO("BST delete root with one child", "[delete]") {
-    tree_t<int> tree = {8, 4, 3};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree == tree_t<int>({4, 3}) );
-}
-
-SCENARIO("BST delete root with children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree == tree_t<int>({10, 9, 4, 3, 13, 11, 12}) );
-}
-
-SCENARIO("BST delete non root without children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(3) );
-    REQUIRE( tree == tree_t<int>({8, 4, 10, 9, 13, 11, 12}) );
-}
-
-SCENARIO("BST delete non root with one child", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(11) );
-    REQUIRE( tree == tree_t<int>({8, 4, 3, 10, 9, 13, 12}) );
-}
-
-SCENARIO("BST delete non root with children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(10) );
-    REQUIRE( tree == tree_t<int>({8, 4, 3, 13, 11, 12, 9}) );
+TEST_CASE("isLeaf tree")
+{
+	tree_t<int> tree;
+	
+	tree.insert(5);
+	tree.insert(7);
+	
+	REQUIRE( tree.isLeaf(root_) == false );
+	REQUIRE( tree.isLeaf(root_->right) == true );
 }
